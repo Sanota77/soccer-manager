bccb084adc93db2728d47d167bfa8dc27576de70
Franck ROTH <Franck.ROTH@ecam-strasbourg.eu>
2022-03-25 16:49:23 +0100

    terrain de merde

--- joueur.php
+++ joueur.php
@@ -14,9 +14,7 @@
   <body>
     <div class="container">
         <div class="row">
-            <div class="col">
-            vide
-            </div>
+            <div class="col"></div>
             <div class="col-6">
                 <!-- NAV -->
                 <ul class="nav">
@@ -24,43 +22,19 @@
                         <a class="nav-link" href="index.php">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">joueurs</a>
+                        <a class="nav-link" href="joueur.php">joueurs</a>
                     </li>
                 </ul>
             <!-- NAV FIN -->
 
-                <h1>Soccer manager !</h1>
+            <h1>Soccer manager !</h1>
 
-                
-                
             </div>
-            <div class="col">
-            vide
-            </div>
+            <div class="col"></div>
         </div>
-    </div>
-
-    <!--affichage terrain -->
-    <div class="terrain">
-        <div class="gardien" style="width: 5rem;">
-        <img src="..." class="card-img-top" alt="gardien de but">
-            <div class="card-body">
-                <h5 class="card-title">Nom prenom</h5>
-            </div>
-        </div>
-    <div class="def1">defenseur</div>
-    <div class="def2">defenseur</div>
-    <div class="def3">defenseur</div>
-    <div class="def4">defenseur</div>
-    <div class="mid1">milieu</div>
-    <div class="mid2">milieu</div>
-    <div class="mid3">milieu</div>
-    <div class="att1">Attaquant</div>
-    <div class="att2">Attaquant</div>
-    <div class="att3">Attaquant</div>
     </div>
     
-
+    
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
